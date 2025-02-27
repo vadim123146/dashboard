f9b1a1bde46075b777f6276de9bac30a88671698
vadim123146 <lukancevvadim079@gmail.com~>
2025-02-27 21:29:28 +0300

    3

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
 module.exports = {
- publicPath: process.env.NODE_ENV === 'production' ? '/dashboard/' : '/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/dashboard/'
+    : '/'
 }
